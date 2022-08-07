f102f89ef25be66ffbfdef3f33f4b3bc6c2345f6
Jhonathan <jhon.1307@outlook.com>
2022-08-07 14:10:22 -0500

    servicio sms

--- src/app/auth/doble-auth/doble-auth.component.ts
+++ src/app/auth/doble-auth/doble-auth.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/rest/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-doble-auth',
@@ -8,11 +11,47 @@ import { Router } from '@angular/router';
 })
 export class DobleAuthComponent implements OnInit {
 
-  constructor( private router: Router ) { }
+  constructor( private authService: AuthService, private router: Router ) { }
+
+  loginForm: FormGroup;
 
   ngOnInit(): void {
 
-    this.router.navigate(['/']);
+    this.formControl();
+ 
   }
 
+  formControl(): void {
+   this.loginForm = new FormGroup({
+   code: new FormControl('', [ Validators.required,]),
+    
+   });
+    
+  }
+
+  get error(): any {
+    return this.loginForm.controls;
+  }
+
+  authSms(loginForm) {
+
+    const user = new FormData();
+    
+    user.append('code', loginForm.value.code);
+
+    this.authService.dobleAuth(user).subscribe((resp)=>{
+
+      localStorage.setItem('user', resp.access_token);
+
+      this.router.navigate(['/']);
+
+    }, error=>{
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'No se ha podido autenticar su usuario!',
+      })
+    }
+  )}
 }
